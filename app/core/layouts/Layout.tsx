4475887e0895ee37727dc52cc9523831bd367acc
Vijay Shanmugam <36575091+Jbolt01@users.noreply.github.com>
2021-10-29 21:37:27 -0400

    Update Layout.tsx

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -13,6 +13,7 @@ const Layout = ({ title, children }: LayoutProps) => {
       <Head>
         <title>{title || "Blair Lunch Activities"}</title>
         <link rel="icon" href="mbhslunch.svg" sizes="any" type="image/svg+xml" />
+        <script async defer data-website-id="2f5684c8-9119-4bd9-b50d-a4b63c7d8aa1" src="https://umami.vijayrs.com/umami.js"></script>
       </Head>
       <Navbar />
       {children}
